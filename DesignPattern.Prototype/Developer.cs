b0648d5722b277cfa006ba7f9754528fdfb8d540
Arash Mehdipour <arash201221@gmail.com>
2021-10-15 10:35:41 +0330

    DesignPattern Prototype

---
+++ DesignPattern.Prototype/Developer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Prototype
+{
+    public class Developer : Employee
+    {
+        public string Language { get; set; }
+        public override Employee Clone()
+        {
+            return (Employee)MemberwiseClone();
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0},{1},{2}",Name,Age,Language);
+        }
+    }
+}
---
+++ DesignPattern.Prototype/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DesignPattern.Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Developer developer = new Developer();
+            developer.Name = "Arash";
+            developer.Age = 28;
+            developer.Language = "C#";
+
+            Developer developerCopy = (Developer) developer.Clone();
+            developerCopy.Name = "Ali";
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

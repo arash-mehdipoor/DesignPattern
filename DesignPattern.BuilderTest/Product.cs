0c88093a45ffd0e7b2da268c33643137b4379f83
Arash Mehdipour <arash201221@gmail.com>
2021-10-15 09:48:22 +0330

    Desgin Pattern Builder 2

---
+++ DesignPattern.BuilderTest/Product.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.BuilderTest
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Brand { get; set; }
+
+        public Product(int id, string name, double price, string brand)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Brand = brand;
+        }
+    }
+    public abstract class BuilderPattern<T>
+    {
+        public abstract T Build();
+    }
+
+    public class ProductBuilder : BuilderPattern<Product>
+    {
+        public int Id = 1;
+        public string Name = "Product 1";
+        public double Price = 23000;
+        public string Brand = "Brand 1";
+
+        public override Product Build()
+        {
+            var t = this;
+            return new Product(Id, Name, Price, Brand);
+        }
+
+        public ProductBuilder WithName(string name)
+        {
+            this.Name = name;
+            return this;
+        }
+
+        public ProductBuilder WithPrice(double price)
+        {
+            this.Price = price;
+            return this;
+        }
+
+        public ProductBuilder WithBrand(string brand)
+        {
+            this.Brand = brand;
+            return this;
+        }
+    }
+}
---
+++ DesignPattern.BuilderTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DesignPattern.BuilderTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ProductBuilder();
+
+            var product1 = builder.Build();
+
+            var product2 = builder.WithPrice(3000).Build();
+
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}

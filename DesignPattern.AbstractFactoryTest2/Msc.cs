0345590bbefb87f3a205c41519d9f2952d1f4f4f
Arash Mehdipour <arash201221@gmail.com>
2021-10-16 22:13:48 +0330

    DesignPattern.AbstractFactoryTest2

---
+++ DesignPattern.AbstractFactoryTest2/Msc.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.AbstractFactoryTest2
+{
+    //1
+    public abstract class Packaging { }
+    public class StandardPackaging : Packaging { };
+    public class ShockProofPackaging : Packaging { };
+
+
+    //1
+    public abstract class DeliveryDocument { }
+    public class PostalLabel : DeliveryDocument { }
+    public class CourierManifest : DeliveryDocument { }
+
+
+    //2
+    public abstract class PurchaseFactory
+    {
+        public abstract Packaging CreatePackaging();
+        public abstract DeliveryDocument CreateDeliveryDocument();
+    }
+
+
+    //3
+    public class DelicatePurchaseFactory : PurchaseFactory
+    {
+        public override DeliveryDocument CreateDeliveryDocument()
+        {
+            return new CourierManifest();
+        }
+
+        public override Packaging CreatePackaging()
+        {
+            return new StandardPackaging();
+        }
+    }
+
+    //3
+    public class StandardPurchaseFactory : PurchaseFactory
+    {
+        public override DeliveryDocument CreateDeliveryDocument()
+        {
+            return new PostalLabel();
+        }
+
+        public override Packaging CreatePackaging()
+        {
+            return new ShockProofPackaging();
+        }
+    }
+
+    //4 
+    public class Client
+    {
+        private Packaging _packaging;
+        private DeliveryDocument _deliveryDocument;
+
+        public Client(PurchaseFactory factory)
+        {
+            _packaging = factory.CreatePackaging();
+            _deliveryDocument = factory.CreateDeliveryDocument();
+        }
+
+        public Packaging ClientPackaging
+        {
+            get { return _packaging; }
+        }
+
+        public DeliveryDocument ClientDocument
+        {
+            get { return _deliveryDocument; }
+        }
+    }
+}
---
+++ DesignPattern.AbstractFactoryTest2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DesignPattern.AbstractFactoryTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PurchaseFactory spf = new StandardPurchaseFactory();
+            Client standard = new Client(spf);
+
+            Console.WriteLine(standard.ClientPackaging.GetType());
+            Console.WriteLine(standard.ClientDocument.GetType());
+
+
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
